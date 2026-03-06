e9766ba404689bbd311704e99feee507763713b8
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2026-03-06 17:08:56 +0100

    prueba node 22

--- conex.js
+++ conex.js
@@ -8,3 +8,27 @@ const uri = `mongodb+srv://${user}:${password}@cluster0.0rq4uy9.mongodb.net/${db
 mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>console.log('conectado a mongodb'))
     .catch((e)=> console.log('error de conexion', e));
+
+
+//     import mongoose from 'mongoose';
+// import dns from 'dns';
+
+// dns.setDefaultResultOrder('ipv4first'); // ← Agrega esta línea
+
+// export const connect = async () => {
+//   try {
+//     const bd = await mongoose.connect(process.env.DB_URL);
+//     const { name, host } = bd.connection;
+//     console.log(`Base de datos conectada: ${name} en el host: ${host}`);
+//   } catch (error) {
+//     console.log('Error al conectar a la base de datos:', error.message);
+//     process.exit(1);
+//   }
+// };
+
+
+///////////////////////////////////////////////
+// const bd = await mongoose.connect(process.env.DB_URL, {
+//   family: 4,
+//   serverSelectionTimeoutMS: 5000,
+// });
