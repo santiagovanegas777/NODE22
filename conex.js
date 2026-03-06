6d8458a30241bddbd141135bf694cb6050079376
correavanegassantiago777@gmail.com <correavanegassantiago777@gmail.com>
2026-03-06 17:28:55 +0100

    prueba node 22

--- conex.js
+++ conex.js
@@ -1,34 +1,36 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+
+
+
 
 const user = 'santi34';
 const password = 'valencia6';
-const db = 'goodappetite';
-const uri = `mongodb+srv://${user}:${password}@cluster0.0rq4uy9.mongodb.net/${db}?appName=Cluster0`
+const db = 'alumnos';
+//const uri = `mongodb+srv://${user}:${password}@cluster0.0rq4uy9.mongodb.net/${db}?appName=Cluster0`
 
-mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>console.log('conectado a mongodb'))
-    .catch((e)=> console.log('error de conexion', e));
+// mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true})
+//     .then(()=>console.log('conectado a mongodb'))
+//     .catch((e)=> console.log('error de conexion', e));
 
+import mongoose from 'mongoose';
+import { setDefaultResultOrder } from 'dns';
 
-//     import mongoose from 'mongoose';
-// import dns from 'dns';
+setDefaultResultOrder('ipv4first');
 
-// dns.setDefaultResultOrder('ipv4first'); // ← Agrega esta línea
+export const connect = async () => {
+  try {
+    console.log('Conectando a la base de datos...'); // ← ver si llega aquí
+    
+    const bd = await mongoose.connect(process.env.DB_URL, {
+      family: 4,
+      serverSelectionTimeoutMS: 5000,
+      connectTimeoutMS: 5000,
+    });
 
-// export const connect = async () => {
-//   try {
-//     const bd = await mongoose.connect(process.env.DB_URL);
-//     const { name, host } = bd.connection;
-//     console.log(`Base de datos conectada: ${name} en el host: ${host}`);
-//   } catch (error) {
-//     console.log('Error al conectar a la base de datos:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-
-///////////////////////////////////////////////
-// const bd = await mongoose.connect(process.env.DB_URL, {
-//   family: 4,
-//   serverSelectionTimeoutMS: 5000,
-// });
+    const { name, host } = bd.connection;
+    console.log(`Base de datos conectada: ${name} en el host: ${host}`);
+  } catch (error) {
+    console.log('Error:', error.message); // ← ver el error exacto
+    process.exit(1);
+  }
+};
